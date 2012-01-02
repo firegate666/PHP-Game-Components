25158813b2244076d8271586e5c7650e38b44b70
Marco Behnke <marco@behnke.biz>
2012-01-03 00:49:19 +0100

    add getter and setter

--- biz/behnke/game/techtree/TechTree.php
+++ biz/behnke/game/techtree/TechTree.php
@@ -14,5 +14,34 @@ class TechTree {
 	 * @var String
 	 */
 	protected $description;
+	
+	/**
+	 * @return the $name
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @return the $description
+	 */
+	public function getDescription() {
+		return $this->description;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	/**
+	 * @param string $description
+	 */
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+
 
 }
