4e98a5fddbf3c361c81103d20a484170f1bf4f34
Marco Behnke <marco@behnke.biz>
2012-01-03 01:06:20 +0100

    reformat code according to coding guidelines

--- biz/behnke/game/techtree/TechTree.php
+++ biz/behnke/game/techtree/TechTree.php
@@ -1,64 +1,72 @@
 <?php
 /**************************************************************************
  *
-* Copyright 2011 Marco Behnke <marco@behnke.biz>
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-**************************************************************************/
+ * Copyright 2011 Marco Behnke <marco@behnke.biz>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **************************************************************************/
 
 namespace biz\behnke\game\techtree;
 
-class TechTree {
-	
+class TechTree
+{
+
 	/**
-	 * 
+	 *
 	 * @var String
 	 */
 	protected $name;
-	
+
 	/**
-	 * 
+	 *
 	 * @var String
 	 */
 	protected $description;
-	
+
 	/**
+	 *
 	 * @return the $name
 	 */
-	public function getName() {
+	public function getName()
+	{
 		return $this->name;
 	}
 
 	/**
+	 *
 	 * @return the $description
 	 */
-	public function getDescription() {
+	public function getDescription()
+	{
 		return $this->description;
 	}
 
 	/**
-	 * @param string $name
+	 *
+	 * @param $name string       	
 	 */
-	public function setName($name) {
+	public function setName($name)
+	{
 		$this->name = $name;
 	}
 
 	/**
-	 * @param string $description
+	 *
+	 * @param $description string       	
 	 */
-	public function setDescription($description) {
+	public function setDescription($description)
+	{
 		$this->description = $description;
 	}
-
 
 }
--- biz/behnke/game/test/TechTreeTest.php
+++ biz/behnke/game/test/TechTreeTest.php
@@ -1,69 +1,74 @@
 <?php
 /**************************************************************************
  *
-* Copyright 2011 Marco Behnke <marco@behnke.biz>
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-**************************************************************************/
+ * Copyright 2011 Marco Behnke <marco@behnke.biz>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **************************************************************************/
 
 require_once 'PHPUnit/Framework/TestCase.php';
-require_once __DIR__.'/../techtree/TechTree.php';
+require_once __DIR__ . '/../techtree/TechTree.php';
 
 /**
  * test case.
  */
-class TechTreeTest extends PHPUnit_Framework_TestCase {
-	
+class TechTreeTest extends PHPUnit_Framework_TestCase
+{
+
 	/**
-	 * 
+	 *
 	 * @var \biz\behnke\game\techtree\TechTree
 	 */
 	protected $tt;
-	
+
 	/**
 	 * Prepares the environment before running a test.
 	 */
-	protected function setUp() {
+	protected function setUp()
+	{
 		parent::setUp ();
-		$this->tt = new \biz\behnke\game\techtree\TechTree();
+		$this->tt = new \biz\behnke\game\techtree\TechTree ();
 		// TODO Auto-generated TechTreeTest::setUp()
 	}
-	
-	public function testFails() {
-		$this->assertNotEquals(true, true);
+
+	public function testFails()
+	{
+		$this->assertNotEquals ( true, true );
 	}
-	
-	public function testSetName() {
+
+	public function testSetName()
+	{
 		$name = 'My Name';
-		$this->tt->setName($name);
-		$this->assertAttributeSame($name, 'name', $this->tt);
+		$this->tt->setName ( $name );
+		$this->assertAttributeSame ( $name, 'name', $this->tt );
 	}
-	
+
 	/**
 	 * Cleans up the environment after running a test.
 	 */
-	protected function tearDown() {
+	protected function tearDown()
+	{
 		// TODO Auto-generated TechTreeTest::tearDown()
 		
 		parent::tearDown ();
 	}
-	
+
 	/**
 	 * Constructs the test case.
 	 */
-	public function __construct() {
+	public function __construct()
+	{
 		// TODO Auto-generated constructor
 	}
 
 }
-
