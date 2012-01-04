ae818b4bf2231f8ea9d0bf724e3a40d2b9c88110
Marco Behnke <marco@behnke.biz>
2012-01-04 22:53:23 +0100

    add techtree factory and xml loader

---
+++ appBase/cx/php/game/techtree/TechTreeFactory.php
@@ -0,0 +1,55 @@
+<?php
+/**************************************************************************
+ *
+* Copyright 2011-2012 Marco Behnke <marco@php.cx>
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**************************************************************************/
+
+namespace cx\php\game\techtree;
+
+use cx\php\game\component\ComponentLoader;
+
+abstract class TechTreeFactory
+{
+
+	protected static $singleton;
+
+	/**
+	 *
+	 * hide constructor from public
+	 */
+	private function __construct()
+	{
+
+	}
+
+	/**
+	 * @param mixed $source
+	 * @return TechTree
+	 */
+	abstract function loadTree($source = null);
+
+	/**
+	 * @return TechTreeFactory
+	 */
+	public static function getInstance()
+	{
+		if (is_null ( static::$singleton ))
+		{
+			$class_name = get_called_class();
+			static::$singleton = new $class_name();
+		}
+		return static::$singleton;
+	}
+}
---
+++ appBase/cx/php/game/techtree/XMLTechTreeFactory.php
@@ -0,0 +1,72 @@
+<?php
+/**************************************************************************
+ *
+ * Copyright 2011-2012 Marco Behnke <marco@php.cx>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **************************************************************************/
+
+namespace cx\php\game\techtree;
+
+/**
+ * create techtree from xml
+ */
+class XMLTechTreeFactory extends TechTreeFactory
+{
+
+	/**
+	 *
+	 * @see cx\php\game\techtree.TechTreeFactory::loadTree()
+	 *
+	 * @param $source String
+	 *       	 path to XML
+	 * @throws TechTreeItemExistsException
+	 * @return TechTree
+	 */
+	public function loadTree($source = null)
+	{
+		$xml = new \SimpleXMLElement($source, 0, true);
+		$tt = new TechTree();
+		$tt->setName((string)$xml['name']);
+		$tt->setDescription((string)$xml->description);
+		
+		foreach($xml->techtreeitem as $entryXml){
+			$tt->addItem($this->getTechTreeItem($entryXml)); // TechTreeItemExistsException
+		}
+		
+		return $tt;
+	
+	}
+
+	/**
+	 * create techtree item
+	 * 
+	 * @param \SimpleXMLElement $xml
+	 * @return TechTreeItem
+	 */
+	protected function getTechTreeItem(\SimpleXMLElement $xml)
+	{
+		$tti = new TechTreeItem();
+		$tti->setKey((string)$xml['key'])
+			->setName((string)$xml['name'])
+			->setDescription((string)$xml->description);
+		
+		foreach($xml->dependson as $dependency)
+		{
+			$tti->addDependency((string)$dependency['key']);
+		}
+
+		return $tti;
+	}
+
+}
