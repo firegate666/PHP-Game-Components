e74c4ecbb33bb0628c287e732b1cf0c288f11bd1
Marco Behnke <marco@behnke.biz>
2012-12-05 23:59:55 +0100

    oop redesign

--- example/sprites/js/spritemanager.js
+++ example/sprites/js/spritemanager.js
@@ -1,75 +1,76 @@
-function SpriteManager() {
+var SpriteManager = function() {
 
-	var sprites = new Array();
-
-	/**
-	 * add sprite to collection
-	 *
-	 * @return void
-	 */
-	this.addSprite = function(sprite) {
-		sprite.setManager(this);
-		sprites.push(sprite);
-	};
-
-	/**
-	 * set player sprite that reacts on the mouse
-	 *
-	 * @param {Sprite} next_player
-	 * @return void
-	 */
-	this.setPlayer = function(next_player) {
-		$.each(sprites, function(k, v) {
-			v.setPlayer(false);
-
-			if (v == next_player) {
-				next_player = k;
-			}
-		});
-
-		sprites[next_player].setPlayer(true);
-	}
-
-	/**
-	 * print out manager status
-	 *
-	 * @return void
-	 */
-	this.status = function() {
-		var status = {sprites: {}};
-		$.each(sprites, function(k, sprite) {
-			status.sprites[k] = {isPlayer: sprite.isPlayer(), object: sprite};
-		});
-		return status;
-	}
+	this.sprites = new Array();
 
 	this.animation_loop_update = 0;
 
-	/**
-	 * update loop
-	 *
-	 * @return void
-	 */
-	this.updateSprites = function() {
-		if (this.animation_loop_update === 0) {
-			this.animation_loop_update = new Date().getTime();
+}
+
+/**
+ * add sprite to collection
+ *
+ * @return void
+ */
+SpriteManager.prototype.addSprite = function(sprite) {
+	sprite.setManager(this);
+	this.sprites.push(sprite);
+};
+
+/**
+ * set player sprite that reacts on the mouse
+ *
+ * @param {Sprite} next_player
+ * @return void
+ */
+SpriteManager.prototype.setPlayer = function(next_player) {
+	$.each(this.sprites, function(k, v) {
+		v.setPlayer(false);
+
+		if (v == next_player) {
+			next_player = k;
 		}
+	});
 
-		var self = this;
+	this.sprites[next_player].setPlayer(true);
+}
 
-		window.requestAnimFrame(function() {
-			var current = new Date().getTime(),
-				delta = current - self.animation_loop_update;
+/**
+ * print out manager status
+ *
+ * @return void
+ */
+SpriteManager.prototype.status = function() {
+	var status = {sprites: {}};
+	$.each(this.sprites, function(k, sprite) {
+		status.sprites[k] = {isPlayer: sprite.isPlayer(), object: sprite};
+	});
+	return status;
+}
 
-			if (delta >= 40) {
-				$.each(sprites, function(k, sprite) {
-					sprite.update();
-				});
-				self.animation_loop_update = new Date().getTime();
-			}
 
-			self.updateSprites();
-		});
+/**
+ * update loop
+ *
+ * @return void
+ */
+SpriteManager.prototype.updateSprites = function() {
+	if (this.animation_loop_update === 0) {
+		this.animation_loop_update = new Date().getTime();
 	}
 
+	var self = this;
+
+	window.requestAnimFrame(function() {
+		var current = new Date().getTime(),
+			delta = current - self.animation_loop_update;
+
+		if (delta >= 40) {
+			$.each(self.sprites, function(k, sprite) {
+				sprite.update();
+			});
+			self.animation_loop_update = new Date().getTime();
+		}
+
+		self.updateSprites();
+	});
 }
