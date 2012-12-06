9310af5931e9af60ac114f19cf1c4707d94d842d
Marco Behnke <marco@behnke.biz>
2012-12-06 10:14:21 +0100

    wrap in closure

--- example/sprites/js/sprite_data.js
+++ example/sprites/js/sprite_data.js
@@ -1,61 +1,63 @@
-var sprites = {
-	flying_soul_blonde : {
-		x: 100,
-		y: 100,
-		facing: 'n',
-		'style' : {
-			width : 128,
-			height : 128
+(function() {
+	window.sprites = {
+		flying_soul_blonde : {
+			x: 100,
+			y: 100,
+			facing: 'n',
+			'style' : {
+				width : 128,
+				height : 128
+			},
+			minFrame : 0,
+			maxFrame : 15,
+			type: 'flying_soul_blonde',
+			player: true,
+			frames_per_second: 15
 		},
-		minFrame : 0,
-		maxFrame : 15,
-		type: 'flying_soul_blonde',
-		player: true,
-		frames_per_second: 15
-	},
 
-	flying_soul_blonde2 : {
-		x: 300,
-		y: 150,
-		facing: 's',
-		'style' : {
-			width : 128,
-			height : 128
+		flying_soul_blonde2 : {
+			x: 300,
+			y: 150,
+			facing: 's',
+			'style' : {
+				width : 128,
+				height : 128
+			},
+			minFrame : 0,
+			maxFrame : 15,
+			type: 'flying_soul_blonde',
+			player: false,
+			frames_per_second: 15
 		},
-		minFrame : 0,
-		maxFrame : 15,
-		type: 'flying_soul_blonde',
-		player: false,
-		frames_per_second: 15
-	},
 
-	flying_soul_blonde3 : {
-		x: 500,
-		y: 50,
-		facing: 'w',
-		'style' : {
-			width : 128,
-			height : 128
+		flying_soul_blonde3 : {
+			x: 500,
+			y: 50,
+			facing: 'w',
+			'style' : {
+				width : 128,
+				height : 128
+			},
+			minFrame : 0,
+			maxFrame : 15,
+			type: 'flying_soul_blonde',
+			player: false,
+			frames_per_second: 15
 		},
-		minFrame : 0,
-		maxFrame : 15,
-		type: 'flying_soul_blonde',
-		player: false,
-		frames_per_second: 15
-	},
 
-	airdragon1 : {
-		x: 600,
-		y: 150,
-		facing: 'e',
-		'style' : {
-			width : 128,
-			height : 128
-		},
-		minFrame : 0,
-		maxFrame : 7,
-		type: 'airdragon',
-		player: false,
-		frames_per_second: 15
+		airdragon1 : {
+			x: 600,
+			y: 150,
+			facing: 'e',
+			'style' : {
+				width : 128,
+				height : 128
+			},
+			minFrame : 0,
+			maxFrame : 7,
+			type: 'airdragon',
+			player: false,
+			frames_per_second: 15
+		}
 	}
-}
+})();
