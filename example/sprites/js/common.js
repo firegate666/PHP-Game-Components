d5590f0ad35943cbf6de1baa78857a27710a6779
Marco Behnke <marco@behnke.biz>
2012-12-06 09:25:07 +0100

    indentation

--- example/sprites/js/common.js
+++ example/sprites/js/common.js
@@ -1,12 +1,13 @@
 window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-		  window.webkitRequestAnimationFrame ||
-		  window.mozRequestAnimationFrame    ||
-		  window.oRequestAnimationFrame      ||
-		  window.msRequestAnimationFrame     ||
-		  function( callback ){
+	"use strict";
+	return window.requestAnimationFrame       ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		window.oRequestAnimationFrame      ||
+		window.msRequestAnimationFrame     ||
+		function( callback ){
 			window.setTimeout(callback, 1000 / 60);
-		  };
+		};
 })();
 
 /**
