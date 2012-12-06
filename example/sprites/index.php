df710e5aea669a3d36c973fe93677eaa5cdbaef5
Marco Behnke <marco@behnke.biz>
2012-12-06 13:12:27 +0100

    add minified js

--- example/sprites/index.php
+++ example/sprites/index.php
@@ -1,3 +1,21 @@
+<?php
+
+	require_once __DIR__ . '/class.JavaScriptPacker.php';
+
+	function packJS($js_infile, $js_outfile) {
+
+		if (!file_exists($js_outfile) || filemtime($js_outfile) < filemtime($js_infile)) {
+			$packer = new JavaScriptPacker(file_get_contents($js_infile), 'Normal', true, false);
+			file_put_contents($js_outfile, $packer->pack());
+		}
+	}
+
+	packJS(__DIR__ . '/js/common.js', __DIR__ . '/js/min/common.min.js');
+	packJS(__DIR__ . '/js/sprite_data.js', __DIR__ . '/js/min/sprite_data.min.js');
+	packJS(__DIR__ . '/js/spritemanager.js', __DIR__ . '/js/min/spritemanager.min.js');
+	packJS(__DIR__ . '/js/sprite.js', __DIR__ . '/js/min/sprite.min.js');
+	packJS(__DIR__ . '/js/game.js', __DIR__ . '/js/min/game.min.js');
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -22,10 +40,10 @@
 			<script type="text/javascript" src="js/game.js"></script>
 		<?php else: ?>
 			<script type="text/javascript" src="<?= file_exists('js/min/common.min.js') ? 'js/min/common.min.js' : 'js/common.js' ?>"></script>
-			<script type="text/javascript" src="<?= file_exists('js/min/common.min.js') ? 'js/min/sprite_data.min.js' : 'js/sprite_data.js' ?>"></script>
-			<script type="text/javascript" src="<?= file_exists('js/min/common.min.js') ? 'js/min/spritemanager.min.js' : 'js/spritemanager.js' ?>"></script>
-			<script type="text/javascript" src="<?= file_exists('js/min/common.min.js') ? 'js/min/sprite.min.js' : 'js/sprite.js' ?>"></script>
-			<script type="text/javascript" src="<?= file_exists('js/min/common.min.js') ? 'js/min/game.min.js' : 'js/game.js' ?>"></script>
+			<script type="text/javascript" src="<?= file_exists('js/min/sprite_data.min.js') ? 'js/min/sprite_data.min.js' : 'js/sprite_data.js' ?>"></script>
+			<script type="text/javascript" src="<?= file_exists('js/min/spritemanager.min.js') ? 'js/min/spritemanager.min.js' : 'js/spritemanager.js' ?>"></script>
+			<script type="text/javascript" src="<?= file_exists('js/min/sprite.min.js') ? 'js/min/sprite.min.js' : 'js/sprite.js' ?>"></script>
+			<script type="text/javascript" src="<?= file_exists('js/min/game.min.js') ? 'js/min/game.min.js' : 'js/game.js' ?>"></script>
 		<?php endif; ?>
 
 	</head>
